1ad92ef881ee3eb40a4556b053fc9599cad4b45a
mrkish <mark@jhns.in>
2018-09-09 22:48:18 -0500

    Initial commit

---
+++ src/main/java/com/shipsee/ShipseeApplication.java
@@ -0,0 +1,12 @@
+package com.shipsee.shipsee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShipseeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShipseeApplication.class, args);
+	}
+}
---
+++ src/test/java/com/shipsee/shipsee/ShipseeApplicationTests.java
@@ -0,0 +1,16 @@
+package com.shipsee.shipsee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ShipseeApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
